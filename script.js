3e7d90ff068d65108d00bd258a128694d2be222c
ink <92540444+GreatLoveV@users.noreply.github.com>
2024-05-19 16:13:22 +0100

    Declared variables for the calculation components, first number operator and second number respectively

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function calcMult(num1, num2){
 function calcDiv(num1 , num2){
     return num1 / num2
 }
+
+let firstNum = ''
+let operator = ''
+let secondNum = ''
+
+function calculateNums(firstNumber, operatorSign , secondNumber){
+    const num1 = parseFloat(firstNumber);
+    const num2 = parseFloat(secondNumber)
+    if (operatorSign === "+"){
+        return calcAdd(num1 , num2)
+    }else if (operatorSign === "-"){
+        return calcSub(num1 ,num2)
+    }else if (operatorSign === "*"){
+        return calcMult(num1 ,num2)
+    }else if (operatorSign === "/"){
+        return calcDiv(num1 ,num2)
+    }else{
+        return "ERROR: Invalid Operator"
+    }}
+
+firstNum = '20'
+secondNum = '23'
+operator = '*'
+console.log(calculateNums(firstNum, operator , secondNum))
